43dc3b162fb58d7ad042278c857c63eb4e14e386
unknown <andreschererkunzt@gmail.com>
2024-12-12 20:25:40 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Título'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/AndreSchererKunz" />
-    <Titulo fontSize={20}>André Scherer Kunz</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>André Scherer Kunz</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        andreschererkunz
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
